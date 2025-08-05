8abb9a7b81f02eb1bdcda3ae39d915d56fc36127
Tomáš Okruhlica <tomas.okruhlica@siemens-energy.com>
2025-08-05 09:53:22 +0200

    Added Product model and product data in resources

--- src/main/java/tomykulak/furniturestore/model/Product.java
+++ src/main/java/tomykulak/furniturestore/model/Product.java
@@ -1,5 +1,22 @@
 package tomykulak.furniturestore.model;
 
+import lombok.Data;
+import lombok.NonNull;
 
+@Data
 public class Product {
+    @NonNull
+    Integer item_id;
+    String name;
+    String category;
+    Float price;
+    Float old_price;
+    boolean sellable_online;
+    String link;
+    boolean other_colors;
+    String short_description;
+    String designer;
+    Integer depth;
+    Integer height;
+    Integer width;
 }
