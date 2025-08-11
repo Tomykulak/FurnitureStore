7143f28f9e044b77afff2b1da3e626bbc1798c36
Tomáš Okruhlica <tomas.okruhlica@siemens-energy.com>
2025-08-11 08:34:22 +0200

    Added universal notFoundException class

--- src/main/java/tomykulak/furniturestore/exception/NotFoundException.java
+++ src/main/java/tomykulak/furniturestore/exception/NotFoundException.java
@@ -1,4 +1,7 @@
 package tomykulak.furniturestore.exception;
 
-public class NotFoundException {
-}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{}
