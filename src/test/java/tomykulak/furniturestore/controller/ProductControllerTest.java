f3597286511fe9112623e7e5d1920b905ba042bb
Tomáš Okruhlica <tomas.okruhlica@siemens-energy.com>
2025-08-11 09:10:33 +0200

    Added Autogenerated templates for tests

---
+++ src/test/java/tomykulak/furniturestore/controller/ProductControllerTest.java
@@ -0,0 +1,40 @@
+package tomykulak.furniturestore.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProductById() {
+    }
+}
