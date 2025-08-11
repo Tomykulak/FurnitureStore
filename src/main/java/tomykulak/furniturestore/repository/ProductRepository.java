086ccb9ab14ab1683f450fa5de2f80ff39fe2f40
Tomáš Okruhlica <tomas.okruhlica@siemens-energy.com>
2025-08-11 08:35:16 +0200

    Refactored UUID instead of int for ID

--- src/main/java/tomykulak/furniturestore/repository/ProductRepository.java
+++ src/main/java/tomykulak/furniturestore/repository/ProductRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tomykulak.furniturestore.model.Product;
 
+import java.util.UUID;
+
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    boolean existsById(UUID id);
 }
