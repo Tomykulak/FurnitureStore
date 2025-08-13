9d3a5d4568dfe69af807fe4ca1b4571645407ca2
Tomáš Okruhlica <tomas.okruhlica@siemens-energy.com>
2025-08-13 09:41:20 +0200

    Refactor removed unncessary @autowired

--- src/main/java/tomykulak/furniturestore/service/ProductService.java
+++ src/main/java/tomykulak/furniturestore/service/ProductService.java
@@ -14,8 +14,7 @@ import java.util.UUID;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
-
-    @Autowired
+    
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
